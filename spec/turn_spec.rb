41b123ad142bed1cbb20a1a0d45aac922b3f3785
Sam Iavri <sammi80134@gmail.com>
2021-11-30 18:24:34 -0700

    Card class and Turn class made... wrtinging in feedback

---
+++ spec/turn_spec.rb
@@ -0,0 +1,66 @@
+require './lib/turn'
+require './lib/card'
+
+RSpec.describe Card do
+  it 'exists' do
+    card = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
+    turn = Turn.new('Juneau', card)
+    expect(turn).to be_instance_of(Turn)
+    expect(card).to be_instance_of(Card)
+  end
+
+  it 'has a card' do
+    card = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
+    turn = Turn.new('Juneau', card)
+    expect(turn.card).to eq(card)
+  end
+
+  it 'has a guess' do
+    card = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
+    turn = Turn.new('Juneau', card)
+    expect(turn.guess).to eq('Juneau')
+  end
+
+  it 'has knows if the answer is correct' do
+    card = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
+    turn = Turn.new('Juneau', card)
+    expect(turn.correct?).to be true
+  end
+
+  it 'gives feedback' do
+    card = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
+    turn = Turn.new('Juneau', card)
+    expect(turn.feedback).to eq('Correct Broski!!!')
+  end
+
+  it 'is a new card' do
+    card = Card.new('Which planet is closest to the sun?', 'Mercury', :STEM)
+    turn = Turn.new('Saturn', card)
+    expect(turn).to be_instance_of(Turn)
+    expect(card).to be_instance_of(Card)
+  end
+
+  it 'has a card' do
+    card = Card.new('Which planet is closest to the sun?', 'Mercury', :STEM)
+    turn = Turn.new('Saturn', card)
+    expect(turn.card).to eq(card)
+  end
+
+  xit 'has a guess' do
+    card = Card.new('Which planet is closest to the sun?', 'Mercury', :STEM)
+    turn = Turn.new('Saturn', card)
+    expect(turn.guess).to eq('Juneau')
+  end
+
+  xit 'has knows if the answer is correct' do
+    card = Card.new('Which planet is closest to the sun?', 'Mercury', :STEM)
+    turn = Turn.new('Saturn', card)
+    expect(turn.correct?).to be true
+  end
+
+  xit 'gives feedback' do
+    card = Card.new('Which planet is closest to the sun?', 'Mercury', :STEM)
+    turn = Turn.new('Saturn', card)
+    expect(turn.feedback).to eq('Correct Broski!!!')
+  end
+end
